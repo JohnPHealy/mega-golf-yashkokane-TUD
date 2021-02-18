48bcd1e6fe9fb6b228cd6c10a44d279d730686ff
yashkokane-TUD <77935215+yashkokane-TUD@users.noreply.github.com>
2021-02-18 10:36:43 +0000

    added project V1

---
+++ Yash( Mini-Golf)/Assets/Scripts/levelController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
